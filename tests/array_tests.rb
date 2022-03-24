4047f37f1bf00a4bb721bd26f3d8e04ccb601a3d
Malik Kennedy <mksybr@gmail.com>
2022-03-24 17:22:36 -0400

    test: boilerplate cffi

--- tests/array_tests.rb
+++ tests/array_tests.rb
@@ -4,8 +4,20 @@ require 'ffi'
 
 module OSSU
   extend FFI::Library
-  ffi_lib 'c'
-  attach_function :puts, [ :string ], :int
+  ffi_lib "./libossu-arrays.so"
+  attach_function :make_vector, [ :size_t ], :pointer
+  attach_function :destroy_vector, [ :pointer ], :void
+  attach_function :at, [ :size_t ], :size_t
+  attach_function :size, [ :pointer ], :size_t
+  attach_function :capacity, [ :pointer ], :size_t
+  attach_function :is_empty, [ :pointer ], :bool
+  attach_function :push, [ :pointer, :size_t ], :pointer
+  attach_function :push_tail, [ :pointer, :size_t ], :pointer
+  attach_function :pop, [ :pointer ], :pointer
+  attach_function :pop_head, [ :pointer ], :pointer
+  attach_function :insert, [ :pointer ], :pointer
+  attach_function :delete, [ :pointer ], :pointer
+
 end
 
-OSSU.puts 'Hello, World using libc!'
+c_vec = OSSU.make_vector(10)
